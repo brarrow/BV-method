a8e08a8ec89cd6568d5af801b42ee283a4f396db
ielay <ielay@yandex.ru>
2017-10-10 00:12:33 +0300

    add new constructor and generateGraph method for the issue

--- src/main/MatrixGraph.java
+++ src/main/MatrixGraph.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Random;
+
 /**
  * Created by lelay on 09.10.17.
  */
@@ -9,6 +11,28 @@ public class MatrixGraph {
 
     public MatrixGraph(double[][] matrixGraph) {
         this.matrixGraph = matrixGraph;
+    }
+
+    public MatrixGraph(int cityNumber) {
+        this.matrixGraph = generateGraph(cityNumber);
+    }
+
+    public double[][] generateGraph(int cityNumber, int delta) {
+        double[][] matrGr = new double[cityNumber][cityNumber];
+
+        Random rand = new Random();
+
+        for(int i = 0; i < cityNumber; ++i) {
+            matrGr[i][(i + 1) % cityNumber] = rand.nextDouble() * delta;
+            // rand.nextDouble get us the value in borders [0, 1]
+        }
+
+        return matrGr;
+    }
+
+    //defaut value of the delta param is 100
+    public double[][] generateGraph(int cityNumber) {
+        return generateGraph(cityNumber, 100);
     }
 
     public void showMatrixGraph() {
