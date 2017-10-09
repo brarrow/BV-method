a5097a1bbbcf1440085e5ddb38b9224c574a5a11
ielay <ielay@yandex.ru>
2017-10-09 23:45:57 +0300

    add a special class for our project, so it'll be easier to create and use matrix graph. Also added a show method for the issue

--- src/main/MatrixGraph.java
+++ src/main/MatrixGraph.java
@@ -3,5 +3,28 @@ package main;
 /**
  * Created by lelay on 09.10.17.
  */
-public class GraphGenerator {
+public class MatrixGraph {
+
+    private double[][] matrixGraph;
+
+    public MatrixGraph(double[][] matrixGraph) {
+        this.matrixGraph = matrixGraph;
+    }
+
+    public void showMatrixGraph() {
+        for(int i = 0; i < this.matrixGraph.length; ++i) {
+            for(int j = 0; j < this.matrixGraph[0].length; ++j) {
+                System.out.print(this.matrixGraph[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void setWeight(int i, int j, double weight) {
+        this.matrixGraph[i][j] = weight;
+    }
+
+    public double getWeight(int i, int j) {
+        return this.matrixGraph[i][j];
+    }
 }
