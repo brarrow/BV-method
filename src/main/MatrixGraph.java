ee0554136acab086f55b7744d38bec63cbd35631
greenbarrow <green_russia@yahoo.com>
2017-10-16 19:00:29 +0300

    Added Greedy algorithm Changes in Console, GraphReader

--- src/main/MatrixGraph.java
+++ src/main/MatrixGraph.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 /**
@@ -7,52 +9,67 @@ import java.util.Random;
  */
 public class MatrixGraph implements SpecialValues {
 
+    private int cityNumber;
     private double[][] matrixGraph;
 
+    public int getCityNumber() {
+        return cityNumber;
+    }
+
+    public MatrixGraph() {
+
+    }
+
     public MatrixGraph(double[][] matrixGraph) {
+        cityNumber = (int) Math.sqrt(matrixGraph.length);
         this.matrixGraph = matrixGraph;
     }
 
     public MatrixGraph(int cityNumber) {
-        this.matrixGraph = generateGraph(cityNumber);
+        this.cityNumber = cityNumber;
+        matrixGraph = new double[cityNumber][cityNumber];
+        this.generateGraph();
     }
 
-    public double[][] generateGraph(int cityNumber, int delta) {
-        double[][] matrGr = new double[cityNumber][cityNumber];
+    public MatrixGraph(int cityNumber, int delta) {
+        this.cityNumber = cityNumber;
+        matrixGraph = new double[cityNumber][cityNumber];
+        this.generateGraph(delta);
+    }
 
+    public void generateGraph(int delta) {
         Random rand = new Random();
-	
-	//is bi-directional for default
-        for(int i = 0; i < cityNumber; ++i) {
-            for(int j = i+1; j < cityNumber; ++j) {
-                matrGr[i][j] = rand.nextDouble() * delta;
-                // rand.nextDouble get us the value in borders [0, 1]
-                matrGr[j][i] = matrGr[i][j];
-            }
-        }
 
-        return matrGr;
+        //is bi-directional for default
+        for (int i = 0; i < cityNumber; i++) {
+            for (int j = i + 1; j < cityNumber; j++) {
+                matrixGraph[i][j] = new BigDecimal(rand.nextDouble() * delta).setScale(3, RoundingMode.UP).doubleValue();
+                matrixGraph[j][i] = matrixGraph[i][j];
+            }
+            matrixGraph[i][i] = INF;
+        }
     }
 
     //defaut value of the delta param is 100
-    public double[][] generateGraph(int cityNumber) {
-        return generateGraph(cityNumber, 100);
+    public void generateGraph() {
+        this.generateGraph(100);
     }
 
     public void showMatrixGraph() {
-        for(int i = 0; i < this.matrixGraph.length; ++i) {
-            for(int j = 0; j < this.matrixGraph[0].length; ++j) {
-                System.out.print(this.matrixGraph[i][j] + " | ");
+        for (int i = 0; i < cityNumber; i++) {
+            System.out.print(i + 1 + ": ");
+            for (int j = 0; j < cityNumber; j++) {
+                System.out.print(((matrixGraph[i][j] == INF) ? "INF\t" : matrixGraph[i][j]) + "\t| ");
             }
             System.out.println();
         }
     }
 
     public void setWeight(int i, int j, double weight) {
-        this.matrixGraph[i][j] = weight;
+        matrixGraph[i][j] = weight;
     }
 
     public double getWeight(int i, int j) {
-        return this.matrixGraph[i][j];
+        return matrixGraph[i][j];
     }
 }
